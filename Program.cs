b348460f4edebfc15568d87dfbb070bb4e90aecf
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-09-17 15:48:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using CompanyEmployees.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
