d5be10536011420b129853951a7280aa96cc21f7
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-10-06 23:01:02 +0300

    Added AutoMapper to proj, to services, with own logic

--- src/CompanyEmployees/Program.cs
+++ src/CompanyEmployees/Program.cs
@@ -12,6 +12,8 @@ builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
+builder.Services.AddAutoMapper(typeof(Program));
+
 
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly); 
