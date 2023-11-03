cc40277144d3174b6681552d8353d60eaa4f91ec
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-11-03 21:32:55 +0300

    Added  DeleteEmployeeForCompany action with nessecary code around the chain

--- src/Repository/CompanyRepository.cs
+++ src/Repository/CompanyRepository.cs
@@ -5,10 +5,7 @@ namespace Repository
 {
     public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     {
-        public CompanyRepository(RepositoryContext repositoryContext)
-        : base(repositoryContext)
-        {
-        }
+        public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext) {}
 
         public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
              FindAll(trackChanges)
@@ -19,7 +16,11 @@ namespace Repository
             FindByCondition(c => c.Id.Equals(companyId)!, trackChanges).SingleOrDefault();
 
         public void CreateCompany(Company company) => Create(company);
+        
         public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) => 
             FindByCondition(x => ids.Contains(x.Id), trackChanges).ToList();
+
+        public void DeleteCompany(Company company) => Delete(company);
+
     }
 }
