8e37f6ee666693e6d051615e1ec3fb81e2cb8c9c
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-11-23 23:31:23 +0300

    Added data shaping

--- src/CompanyEmployees/Program.cs
+++ src/CompanyEmployees/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Options;
 using NLog;
+using Service.DataShaping;
+using Shared.DataTransferObjects;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +28,8 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<IDataShaper<EmployeeDto>, DataShaper<EmployeeDto>>();
+
 //builder.Services.AddControllers(config =>
 //{
 //    config.RespectBrowserAcceptHeader = true;
--- src/Service.Contracts/IEmployeeService.cs
+++ src/Service.Contracts/IEmployeeService.cs
@@ -1,12 +1,13 @@
 ﻿using Entities.Models;
 using Shared.DataTransferObjects;
 using Shared.RequestFeatures;
+using System.Dynamic;
 
 namespace Service.Contracts
 {
     public interface IEmployeeService
     {
-        Task <(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges);
+        Task<(IEnumerable<Entity> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges);
 
         Task<EmployeeDto> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges);
 
