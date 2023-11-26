e800739b107a1f7234f0b8c35076f265f560b345
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-11-26 18:14:43 +0300

    Added root controller

--- src/CompanyEmployees/Extensions/ServiceExtensions.cs
+++ src/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -31,6 +31,8 @@ namespace CompanyEmployees.Extensions
                 {
                     systemTextJsonOutputFormatter.SupportedMediaTypes
                         .Add("application/vnd.codemaze.hateoas+json");
+                    systemTextJsonOutputFormatter.SupportedMediaTypes
+                        .Add("application/vnd.codemaze.apiroot+json");
                 }
                 var xmlOutputFormatter = config.OutputFormatters
                     .OfType<XmlDataContractSerializerOutputFormatter>()?
@@ -39,6 +41,8 @@ namespace CompanyEmployees.Extensions
                 {
                     xmlOutputFormatter.SupportedMediaTypes
                         .Add("application/vnd.codemaze.hateoas+xml");
+                    xmlOutputFormatter.SupportedMediaTypes
+                        .Add("application/vnd.codemaze.apiroot+xml");
                 }
             });
         }
